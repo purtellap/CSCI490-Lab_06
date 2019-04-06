06d866d577f1077e7e33490e5270726fc600f3f4
purtellap <purtellap@g.cofc.edu>
2019-04-06 14:56:10 -0500

    done

--- app/src/main/java/com/austin/lab_06/MainActivity.java
+++ app/src/main/java/com/austin/lab_06/MainActivity.java
@@ -1,14 +1,19 @@
 package com.austin.lab_06;
 
 import android.arch.persistence.room.Room;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.austin.lab_06.data.LabDatabase;
+import com.austin.lab_06.entities.Person;
+
+import java.util.ArrayList;
 
 import static android.widget.Toast.makeText;
 
@@ -35,18 +40,64 @@ public class MainActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String s = editText.getText().toString();
-                Toast toast = makeText(getApplicationContext(),s,Toast.LENGTH_SHORT);
-                toast.show();
+                final String name = editText.getText().toString();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Person person = new Person();
+                        person.setName(name);
+                        labDB.personDao().insertPerson(person);
+                    }
+                }).start();
             }
         });
         getList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast toast = makeText(getApplicationContext(),"WOrking",Toast.LENGTH_SHORT);
-                toast.show();
+                Retrieval retrieval = new Retrieval(getApplicationContext(), labDB);
+                retrieval.execute();
             }
         });
 
     }
+
+    public static class Retrieval extends AsyncTask<Void, Void, ArrayList<Person>> {
+
+        private LabDatabase labDatabase;
+        Context context;
+
+        Retrieval(Context c, LabDatabase labDB){
+            this.labDatabase = labDB;
+            this.context = c;
+        }
+
+        @Override
+        protected ArrayList<Person> doInBackground(Void... voids) {
+
+            ArrayList<Person> people = (ArrayList<Person>) labDatabase.personDao().getAllPersons();
+            ArrayList<String> names = new ArrayList<>();
+            for (Person p : people){
+                names.add(p.getName());
+            }
+            Intent i = new Intent(context, PersonsActivity.class);
+            i.putExtra("PeopleNames", names);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            try {
+                context.startActivity(i);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+            return people;
+
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Person> people) {
+            super.onPostExecute(people);
+
+        }
+    }
+
 }
--- app/src/main/java/com/austin/lab_06/PersonsActivity.java
+++ app/src/main/java/com/austin/lab_06/PersonsActivity.java
@@ -6,25 +6,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.austin.lab_06.entities.Person;
+
 import java.util.ArrayList;
 
 public class PersonsActivity extends AppCompatActivity {
 
     private ListView listView;
+    private ArrayAdapter<ArrayList<String>> adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_persons);
-
         listView = findViewById(R.id.personName);
 
-        ArrayList<String> i = (ArrayList) this.getIntent().getExtras().get("Persons");
-
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, i);
-
-        listView.setAdapter(adapter);
+        ArrayList<String> i = (ArrayList<String>) getIntent().getExtras().get("PeopleNames");
+        System.out.println(i);
+        if(i != null) {
+            adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, i);
+            listView.setAdapter(adapter);
+        }
 
     }
 }
